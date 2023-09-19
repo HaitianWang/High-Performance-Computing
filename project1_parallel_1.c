74a236651c981d15cef5e19796f5d309dac80d5e
SimonDell <simonqiu4@gmail.com>
2023-09-20 01:23:50 +0800

    fix the bug of rand

--- project1_parallel_1.c
+++ project1_parallel_1.c
@@ -20,13 +20,13 @@
 #include <time.h>
 #include <omp.h>
 
-#define NUM_FISH 10000000        // Number of fish
+#define NUM_FISH 10000000       // Number of fish
 #define WEIGHT 5.0           // Initial weight of fish
 #define W_MAX 10.0      // Maximum weight of fish
 #define POND_SIZE 200   // Pond size
 #define ROUND 10       // Number of rounds
 #define TIMES 5         // number of experiments
-#define NUM_THREADS 8   // Number of THREADS in this process
+#define NUM_THREADS 4   // Number of THREADS in this process
 
 typedef struct {
     double x;
@@ -47,45 +47,65 @@ Timer *performanceTimer;
 double *barycentre;
 int currentRound;
 int currentExperiment;
-unsigned int *rand_seeds;
+//double *experimentTime;
+double averageExperimentTime;
+// unsigned int rand_seeds[NUM_THREADS];
+static unsigned int rand_state[NUM_THREADS];  // Thread local state for random number generation
+#pragma omp threadprivate(rand_state)
+
+
+int rand_threadsafe() {
+    int tid = omp_get_thread_num();
+    rand_state[tid] = (rand_state[tid] * 11035u + 10086u) % ((unsigned)RAND_MAX + 1);
+    return (rand_state[tid] % ((unsigned)RAND_MAX + 1));
+}
 
 // initialize the whole system
 void systemInitialize(){
+
+    srand(time(NULL));
     omp_set_num_threads(NUM_THREADS);  
     school = (Fish *)malloc(sizeof(Fish) * NUM_FISH); 
     performanceTimer = (Timer *)malloc(sizeof(Timer) * TIMES); 
     barycentre = (double *)malloc(sizeof(double) * ROUND); 
     currentRound = 0;
     currentExperiment = 0;
-    rand_seeds = (unsigned int*)malloc(NUM_THREADS * sizeof(unsigned int)); // Seed random number generator 
-    for (int i = 0; i < NUM_THREADS; i++) {
-        rand_seeds[i] = rand();
-    }   
+    averageExperimentTime = 0;
+    //experimentTime = (double *)malloc(sizeof(double) * TIMES); 
 
-    if (!school || !performanceTimer || !barycentre) {
+    if(!school || !performanceTimer || !barycentre) {
         printf("Memory allocation failed!\n");
         exit(1);
     }
 
+    
+    // Seed the random state for each thread
+    #pragma omp parallel
+    {
+        int tid = omp_get_thread_num();
+        rand_state[tid] = rand();
+    }
 }
 
 // initialize the fish school in the pool
-#pragma omp parallel for
 void initializeFish() {
+    #pragma omp parallel for
     for(int i = 0; i < NUM_FISH; i++) {
-        school[i].x = ((double)rand() / RAND_MAX - 0.5) * POND_SIZE;
-        school[i].y = ((double)rand() / RAND_MAX - 0.5) * POND_SIZE;
-        school[i].weight = WEIGHT + ((double)rand() / RAND_MAX - 0.5) * 0.1;
+        int tid = omp_get_thread_num();
+        school[i].x = ((double)rand_threadsafe() / RAND_MAX - 0.5) * POND_SIZE;
+        school[i].y = ((double)rand_threadsafe() / RAND_MAX - 0.5) * POND_SIZE;
+        school[i].weight = WEIGHT + ((double)rand_threadsafe() / RAND_MAX - 0.5) * 0.1;
         school[i].currentObjective = sqrt(school[i].x * school[i].x + school[i].y * school[i].y);
     }
 }
 
 // move the fishes is each round: generate 2 random little numbers and add them in coordinates with both x and y of each fish and calculate the objective function currrently in this round.
-#pragma omp parallel for
 void move() {
+    #pragma omp parallel for
     for(int i = 0; i < NUM_FISH; i++) {
-        school[i].x += (double)rand() / RAND_MAX * 0.2 - 0.1;
-        school[i].y += (double)rand() / RAND_MAX * 0.2 - 0.1;
+        int tid = omp_get_thread_num();
+        school[i].x += (double)rand_threadsafe() / RAND_MAX * 0.2 - 0.1;
+        school[i].y += (double)rand_threadsafe() / RAND_MAX * 0.2 - 0.1;
         school[i].prevObjective = school[i].currentObjective; 
         school[i].currentObjective = sqrt(school[i].x * school[i].x + school[i].y * school[i].y);    
     }
@@ -145,15 +165,19 @@ void optimization() {
 
 void experiment(){
 
+    double totalExperimentTime = 0;
     for (int i = 0; i < TIMES; i++){
         performanceTimer[i].time_start = omp_get_wtime();
         optimization();
         performanceTimer[i].time_end = omp_get_wtime();
         performanceTimer[i].time_duration = performanceTimer[i].time_end - performanceTimer[i].time_start;
-        printf("performanceTimer: %lf\n", performanceTimer[i].time_duration);
+        printf("performance Time: %lf in the Times of %d\n", performanceTimer[i].time_duration, i);
         currentExperiment++;
+        totalExperimentTime +=  performanceTimer[i].time_duration;
     }
-
+    
+    averageExperimentTime = totalExperimentTime/TIMES;
+    printf("The final average performance time is: %lf\n", averageExperimentTime);
 }
 
 
@@ -162,14 +186,19 @@ void freeAll(){
     free(school);  
     free(performanceTimer);
     free(barycentre);
+    //free(rand_seeds);
 }
 
 int main() {
+    printf("1");
     systemInitialize();
+    printf("2");
     initializeFish();
+    printf("3");
     experiment(); 
+    printf("4");
     freeAll();
-    return 0;
+    return 1;
 }
 
 
